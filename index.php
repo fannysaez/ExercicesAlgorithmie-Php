11a4c977f51b7bfaa1f738e0172cb4cfb6a3ba07
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-15 16:46:56 +0100

    initale

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include_once "./include/header.php" ?>
+
+<head>
+    <title>Exercices Algorithmie en Php</title>
+</head>
+
+<body>
+<main>
+      <section class="container">
+        <h1>Exercices Algorithmie en Php</h1>
+      </section>
+
+      <section class="center-exos-container">
+        <p>
+          Test techniques en Php, pas besoin de cloner ce projet La page
+          d'accueil de votre site doit inclure un lien vers les pages dédiées à
+          chaque exercice. 
+        </p>
+        
+        <p>          
+          Hébergez votre projet sur GitHub ou GitLab, selon
+          votre préférence. 
+        </p>
+        <p>Pour chaque exercice, créez une page Php. Assurez-vous que les résultats s’affichent.</p>
+
+        </p>
+        <p><strong>Documentation technique :</strong><br> Dans chaque page Php, listez brièvement les notions ou méthodes
+          Php utilisées pour résoudre l’exercice.<br>
+           
+          Après avoir terminé chaque exercice, effectuez un commit et un push de
+          vos modifications sur le dépôt Git.
+        </p>
+      </section>
+
+      <section class="button-container">
+        <a class="btn-grad" href="exercices/0.1.php">Exercice 0.1</a>
+      </section>
+    </main>
+    
+</body>
+</html>
